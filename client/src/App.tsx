78d1b5cdb5b887cbc1b8ba92caed8626b68aeddf
Alex Swafford <alex_swafford@selinc.com>
2022-04-28 12:22:16 -0400

    Renders a map.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import BingMapsReact from 'bingmaps-react';
+
 import './App.css';
 import bingMapsKey from './bing-maps-key.js';
 
@@ -7,6 +9,18 @@ function App() {
   return (
     <div className="App">
       {bingMapsKey}
+      <BingMapsReact
+      bingMapsKey={bingMapsKey}
+      height="500px"
+      mapOptions={{
+        navigationBarMode: "square",
+      }}
+      width="500px"
+      viewOptions={{
+        center: { latitude: 42.360081, longitude: -71.058884 },
+        mapTypeId: "grayscale",
+      }}
+    />
     </div>
   );
 }
